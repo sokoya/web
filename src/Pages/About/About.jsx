0a6987814adb05b8fd0db964fa398fe32bd1c944
Sokoya Philip <42377896+sokoya@users.noreply.github.com>
2024-10-07 18:11:27 +0100

    Refractored (#56)
    Co-authored-by: AAO001 <kexinlite@gmail.com>

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,85 +1,42 @@
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import About1 from '../../assets copy/images/friends.jpg'
 
-export default function About() {
+function About() {
   return (
-    <Menu as="div" className="absolute inline-block text-left">
-      <div>
-        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900">
-          For Businesses
-        </MenuButton>
-      </div>
+    <>
+      <div className="h-screen bg-sky-700 grid md:grid-cols-2 grid-cols-1 items-center">
+        <div className="flex items-center">
+          <div className="text-white p-2 md:m-5 m-2">
+            <h1 className="font-bold text-xs m-2 my-1">WHO WE ARE</h1>
+            <h1 className="md:text-4xl text-2xl font-bold m-2 my-1 leading-[1.2em]">
+              Transforming Financial Services for Individuals, Businesses, and
+              Beyond.
+            </h1>
+            <p className="m-2 md:leading-7 leading-6">
+              We are on a mission to bridge the gap between individuals and
+              fintech innovation. By offering a platform that serves both
+              personal finance and the sophisticated requirements of businesses,
+              we’re creating a seamless ecosystem where everyone can thrive.
+            </p>
 
-      <MenuItems
-        transition
-        className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-      >
-        <div className="py-1">
-          <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              Edit
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              Duplicate
-            </a>
-          </MenuItem>
+            <div className="my-2 transform hover:-translate-y-1 transition ease-in-out duration-300">
+              <Link
+                to="/register"
+                className="p-3 rounded-lg mx-2 bg-primary text-white"
+              >
+                Create a Free Account
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="py-1">
-          <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              Archive
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              Move
-            </a>
-          </MenuItem>
+
+        <div>
+          <img src={About1} className="h-[80%]"/>
         </div>
-        <div className="py-1">
-          <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              Share
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              Add to favorites
-            </a>
-          </MenuItem>
-        </div>
-        <div className="py-1">
-          <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              Delete
-            </a>
-          </MenuItem>
-        </div>
-      </MenuItems>
-    </Menu>
+      </div>
+    </>
   )
 }
 
+export default About
--- vite.config.js
+++ vite.config.js
@@ -3,36 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react()],
 })
-=======
-    plugins: [react()],
-    manifest: {
-        name: "Payscribe",
-        short_name: "Payscribe",
-        description: "",
-        icons: [{
-                src: "./public/favicon-16x16.png",
-                sizes: "16x16",
-                type: "image/png"
-            },
-            {
-                src: "./public/favicon-32x32.png",
-                sizes: "32x32",
-                type: "image/png"
-            },
-            {
-                src: "./public/mstile-150x150.png",
-                sizes: "150x150",
-                type: "image/png"
-            },
-            {
-                src: "./public/android-chrome-144x144.png",
-                sizes: "144x144",
-                type: "image/png"
-            },
-        ]
-    }
-})
->>>>>>> 1c595f6ec93b5fc282ddda35436453fcce0f1100
