06e8281492ae84a29e21f7b1e1302f5403dba791
AAO001 <kexinlite@gmail.com>
2024-10-07 17:46:16 +0100

    Refractored

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
@@ -9,20 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-=======
-import React from "react"
-import ReactDOM from "react-dom/client"
-
-import AllAppRoutes from "./routes"
-
-const App = () => {
-	return (
-		<div>
-			<AllAppRoutes />
-		</div>
-	)
-}
-
-const root = ReactDOM.createRoot(document.querySelector("#root"))
-root.render(<App />)
->>>>>>> 1c595f6ec93b5fc282ddda35436453fcce0f1100
